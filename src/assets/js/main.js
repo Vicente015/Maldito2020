62fa226805f9ae713e8d8aa0552363ea02c2a757
Vicente <expertope@gmail.com>
2020-06-14 17:00:24 +0000

    Cambiado fondo, creado el carousel, añadidas imágenes de otras series.

---
+++ src/assets/js/main.js
@@ -0,0 +1,4 @@
+const fila = document.querySelector('.contendor-carousel');
+const peliculas = document.querySelector('.pelicula');
+const flechaIzquierda = document.getElementById('flecha-izquierda')
+const flechaDerecha = document.getElementById('flecha-derecha')
